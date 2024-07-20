97b736fac0f91dd6466f906bd6e2e48bb054d798
Haasin-Azeem1 <haasinazeem1@gmail.com>
2024-07-20 18:14:45 +0500

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .sers import *
-from django.contrib.auth.models import User
-from .models import *
-import uuid
 
 # Create your views here.
 @api_view(['POST'])
@@ -13,12 +10,4 @@ def login(request):
 
 @api_view(['POST'])
 def Register(request):
-    email = request.POST['email']
-    number = request.POST['number']
-    password = request.POST['password']
-    session = myuuid = uuid.uuid4()
-
-    user = User.objects.create_user(email, password)
-    user1 = UserModel.objects.create(user_id = user.id, number = number, email = email, session = session)
-
-    return Response(UserSer(user1).data)
+    return Response()
